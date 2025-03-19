9fb84f92df865d23f06425cb01fcaa2c8d4d92f9
Yuvaraj Gunturu <gunturuyuvaraj7995@gmail.com>
2025-03-19 07:18:37 +0530

    Initial Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {Routes, Route  } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import Placeorder from './pages/Placeorder'
+import Navbar from './components/Navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
     <>
+      
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar/>
+        <Routes>
+          <Route path = "/"  element = {<Home/>} />
+          <Route path = "/collection" element = {<Collection/>} />
+          <Route path = "/about" element = {<About/>}  />
+          <Route path = "/cart" element = {<Cart/> } />
+          <Route path = "/contact" element = {<Contact/> } />
+          <Route path = "/login" element = {<Login/> } />
+          <Route path = "/orders" element = {<Orders/> } />
+          <Route path = "/product/:productid" element = {<Product/> } />
+          <Route path = "/place-order" element = {<Placeorder/> } />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/pages/Collection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Collection = () => {
+
+
+return (
+    <div>
+        <h1>Yuvaraj</h1>
+    </div>
+)
+}
+
+export default Collection
